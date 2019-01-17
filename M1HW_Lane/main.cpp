da6c528a2fe460f8247b412b3d676f975b40664a
lanek2228 <46724862+lanek2228@users.noreply.github.com>
2019-01-17 14:00:49 -0500

    The M1HW1 assignment

---
+++ M1HW_Lane/main.cpp
@@ -0,0 +1,23 @@
+// CSC 134
+// M1HW1
+// Khristopher Lane
+// 1/17/19
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // A brief introduction about myself
+
+    cout << "Hello, my name is Khristopher Lane" << endl;
+    cout << "I am working on my Associates of Engineering" << endl;
+    cout << "I had completed my Associates of General Education last year in the summer of " << 2018 << endl;
+    cout << "I love to do " << 2 << " activities on my free time:" << endl;
+    cout << "\t Playing videos games. either PC or console" << endl;
+    cout << "\t Watching movies or shows on Netflix" << endl;
+    cout << endl;
+    cout << "testing\t1\t2\t3" << endl;
+    return 0;
+}
