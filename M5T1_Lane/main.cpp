d32d2d6bee739db67f119fae0693cdcdeda81a6a
lanek2228 <46724862+lanek2228@users.noreply.github.com>
2019-03-07 14:09:26 -0500

    M5T1 assignment

---
+++ M5T1_Lane/main.cpp
@@ -0,0 +1,40 @@
+// CSC 134
+// M5T1
+// March 5, 2019
+// Khristopher Lane
+
+#include <iostream>
+#include "Pen.h"
+using namespace std;
+
+int main()
+{
+    Pen FavoritePen;
+
+    FavoritePen.InkColor = blue;
+    FavoritePen.ShellColor = grey;
+    FavoritePen.CapColor = blue;
+    FavoritePen.Style = ballpoint;
+    FavoritePen.Length = 5.5;
+    FavoritePen.Brand = "Office Depot";
+    FavoritePen.InkLevelPercent = 30;
+
+    Pen WorstPen;
+
+    WorstPen.InkColor = red;
+    WorstPen.ShellColor = red;
+    WorstPen.CapColor = black;
+    WorstPen.Style = fountain_pen;
+    WorstPen.Length = 5.0;
+    WorstPen.Brand = "Parker";
+    WorstPen.InkLevelPercent = 60;
+
+    cout << "This is my favorite pen" << endl;
+    cout << "Color: " << FavoritePen.InkColor << endl; // not suppose to be zero, for that is a placement
+    cout << "Brand: " << FavoritePen.Brand << endl;
+    cout << "Ink Level: " << FavoritePen.InkLevelPercent << "%" << endl;
+
+    FavoritePen.write_on_paper("Hello I am a pen");
+    cout << "Ink Level: " << FavoritePen.InkLevelPercent << "%" << endl;
+    return 0;
+}
